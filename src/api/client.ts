3d7ff0e9cfa0b2bd9da3cb2fb107d5b0ef547fc7
JustTrying652 <ngataevans@gmail.com>
2026-08-15 12:50:25 +0300

    update the api base

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000/api";
+const API_BASE = "http://localhost:8000/api";
 
 async function apiFetch<T>(path: string, options: RequestInit = {}): Promise<T> {
   const res = await fetch(`${API_BASE}${path}`, {
